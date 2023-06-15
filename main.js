5efa9c8f096d3ab1c179411aa65c73081b73b172
Jaspreet <jaspreetkn92@gmail.com>
2023-06-15 18:40:38 +0100

    postioned camera and added cube rotation

--- main.js
+++ main.js
@@ -15,3 +15,15 @@ const geometry = new THREE.BoxGeometry(2, 2, 2);
 const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+camera.position.z = 5;
+
+//Creates loop
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
