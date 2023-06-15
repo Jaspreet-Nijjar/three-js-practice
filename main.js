ba7c2b2ea49c0ac36f6de1c40a3f2ea236711b30
Jaspreet <jaspreetkn92@gmail.com>
2023-06-15 18:36:44 +0100

    added geometry and colour

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
